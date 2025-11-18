845cb01ce529432ec51579c58461f639ef38b090
ErikBerik <erik.j.berglund@hotmail.com>
2025-11-18 17:52:11 +0100

    Added clock, removed debug messages, added logging file to .gitignore

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
         log_state()
@@ -16,6 +15,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
